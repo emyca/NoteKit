9a745b50ccd3fc5497fb46104d8df690ff20ee5a
emyca <evmyca@gmail.com>
2025-08-20 17:58:38 +0300

    Add DI app module

---
+++ app/src/main/java/com/example/notekit/di/AppModule.kt
@@ -0,0 +1,27 @@
+package com.example.notekit.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.notekit.data.NoteDao
+import com.example.notekit.data.NoteDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideNoteDao(noteDatabase: NoteDatabase): NoteDao = noteDatabase.noteDao()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, NoteDatabase::class.java, "notes_db")
+            .build()
+}
