b80483eeb8091b657b50762fc118f19aaf4f4bba
emyca <evmyca@gmail.com>
2025-08-20 17:26:31 +0300

    Add item DAO

---
+++ app/src/main/java/com/example/notekit/data/NoteDao.kt
@@ -0,0 +1,31 @@
+package com.example.notekit.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Query("SELECT * FROM notes")
+    fun getAll(): Flow<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE id=:id")
+    fun getById(id: Int): Flow<Note>
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("DELETE FROM notes WHERE id=:id")
+    suspend fun deleteById(id: Int)
+}
