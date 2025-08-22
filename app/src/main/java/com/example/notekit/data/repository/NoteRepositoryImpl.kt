8693c17385d0c6e52afa374acfac8ff635a2bb81
emyca <evmyca@gmail.com>
2025-08-22 15:29:41 +0300

    Rename repository class

---
+++ app/src/main/java/com/example/notekit/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.example.notekit.data.repository
+
+import com.example.notekit.data.source.local.Note
+import com.example.notekit.data.source.local.NoteDao
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class NoteRepositoryImpl @Inject constructor(private val noteDao: NoteDao) {
+
+    suspend fun insert(note: Note) = noteDao.insert(note)
+
+    fun getAll(): Flow<List<Note>> = noteDao.getAll()
+
+    fun getById(id: Int): Flow<Note> = noteDao.getById(id)
+
+    suspend fun update(note: Note) = noteDao.update(note)
+
+    suspend fun delete(note: Note) = noteDao.delete(note)
+
+    suspend fun deleteById(id: Int) = noteDao.deleteById(id)
+}
