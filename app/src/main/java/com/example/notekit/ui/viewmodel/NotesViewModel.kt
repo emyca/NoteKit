5458d28e529db8725c0b8e7806e08b131e4efc53
emyca <evmyca@gmail.com>
2025-08-27 19:17:20 +0300

    Add note list viewmodel

---
+++ app/src/main/java/com/example/notekit/ui/viewmodel/NotesViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.notekit.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.notekit.domain.model.Note
+import com.example.notekit.domain.usecase.GetAllUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+internal sealed interface NotesScreenUiState {
+    data object Empty: NotesScreenUiState
+    data class Content(val notes: List<Note>): NotesScreenUiState
+}
+
+@HiltViewModel
+internal class NotesViewModel @Inject constructor(
+    getAllUseCase: GetAllUseCase
+) : ViewModel() {}
