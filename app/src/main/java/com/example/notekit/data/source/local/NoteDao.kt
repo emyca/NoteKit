b327e0b7855701aaac2e30620fde980f03346e23
emyca <evmyca@gmail.com>
2025-08-28 18:32:38 +0300

    Clean code

--- app/src/main/java/com/example/notekit/data/source/local/NoteDao.kt
+++ app/src/main/java/com/example/notekit/data/source/local/NoteDao.kt
@@ -6,7 +6,6 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import com.example.notekit.data.source.local.Note
 import kotlinx.coroutines.flow.Flow
 
 @Dao
--- app/src/main/java/com/example/notekit/data/source/local/NoteDatabase.kt
+++ app/src/main/java/com/example/notekit/data/source/local/NoteDatabase.kt
@@ -2,7 +2,6 @@ package com.example.notekit.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.notekit.data.source.local.Note
 
 @Database(entities = [Note::class], version = 1)
 abstract class NoteDatabase : RoomDatabase() {
