46cf7d7b214585606b52fcb6caf287aabae9fc6a
emyca <evmyca@gmail.com>
2025-08-30 19:09:54 +0300

    Change domain logic

--- app/src/main/java/com/example/notekit/domain/usecase/DeleteUseCase.kt
+++ app/src/main/java/com/example/notekit/domain/usecase/DeleteUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.notekit.domain.usecase
 
-import com.example.notekit.data.source.local.Note
+import com.example.notekit.domain.model.Note
 
 interface DeleteUseCase {
     suspend operator fun invoke(note: Note)
