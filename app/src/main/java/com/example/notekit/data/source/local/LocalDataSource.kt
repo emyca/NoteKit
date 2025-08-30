1521bfbe7ab16f5c632079f7f65a44441e2b364e
emyca <evmyca@gmail.com>
2025-08-30 18:49:04 +0300

    Add local data actions set

---
+++ app/src/main/java/com/example/notekit/data/source/local/LocalDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.notekit.data.source.local
+
+import com.example.notekit.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface LocalDataSource {
+
+    suspend fun insert(note: Note)
+
+    fun getAll(): Flow<List<Note>>
+
+    fun getById(id: Int): Flow<Note>
+
+    suspend fun update(note: Note)
+
+    suspend fun delete(note: Note)
+
+    suspend fun deleteById(id: Int)
+}
