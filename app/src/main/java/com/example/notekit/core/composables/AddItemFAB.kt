82d202ef7e34f77a94a8a0623cc92b41f9a22d56
emyca <evmyca@gmail.com>
2025-09-01 14:02:39 +0300

    Add add item FAB composable

---
+++ app/src/main/java/com/example/notekit/core/composables/AddItemFAB.kt
@@ -0,0 +1,38 @@
+package com.example.notekit.core.composables
+
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
+import com.example.notekit.R
+
+@Composable
+fun AddItemFAB(
+    modifier: Modifier = Modifier,
+    containerColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    contentColor: Color = MaterialTheme.colorScheme.primaryContainer,
+    iconTintColor: Color = MaterialTheme.colorScheme.onPrimary,
+    shape: Shape = CircleShape,
+    onClick: () -> Unit
+) {
+    FloatingActionButton(
+        modifier = modifier,
+        onClick = onClick,
+        shape = shape,
+        containerColor = containerColor,
+        contentColor = contentColor
+    ) {
+        Icon(
+            imageVector = Icons.Filled.Add,
+            contentDescription = stringResource(R.string.add_item_fab),
+            tint = iconTintColor
+        )
+    }
+}
