31efdd1d5d816f32322b5a937723e5b3daf3bfea
emyca <evmyca@gmail.com>
2025-09-01 15:25:54 +0300

    Add notes screen navigation

---
+++ app/src/main/java/com/example/notekit/home/presentation/navigation/NotesScreenNavigation.kt
@@ -0,0 +1,26 @@
+package com.example.notekit.home.presentation.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.notekit.home.presentation.NotesScreenRoute
+
+const val NOTES_SCREEN_ROUTE = "notes_screen"
+
+fun NavController.navigateToNotesScreen() = navigate(NOTES_SCREEN_ROUTE) {
+    popUpTo(NOTES_SCREEN_ROUTE) {
+        inclusive = true
+    }
+}
+
+fun NavGraphBuilder.notesScreen(
+    navigateToAddNoteScreen: () -> Unit,
+    navigateToDetailNoteScreen: (String) -> Unit
+) {
+    composable(route = NOTES_SCREEN_ROUTE) {
+        NotesScreenRoute(
+            navigateToAddNoteScreen = navigateToAddNoteScreen,
+            navigateToDetailNoteScreen = navigateToDetailNoteScreen
+        )
+    }
+}
