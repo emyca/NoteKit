e67bae06b9099714b8887ecb9081a634fdae0941
emyca <evmyca@gmail.com>
2025-09-02 13:08:27 +0300

    Update Main activity

--- app/src/main/java/com/example/notekit/MainActivity.kt
+++ app/src/main/java/com/example/notekit/MainActivity.kt
@@ -3,14 +3,7 @@ package com.example.notekit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.notekit.navigation.AppNavigation
 import com.example.notekit.ui.theme.NoteKitTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,32 +11,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            NoteKitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            NoteKitTheme(dynamicColor = false) {
+                AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteKitTheme {
-        Greeting("Android")
     }
 }
