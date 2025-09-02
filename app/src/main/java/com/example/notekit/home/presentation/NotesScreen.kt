b6c1e7d94666ff17d00751dc4cb326177ef35d82
emyca <evmyca@gmail.com>
2025-09-02 12:33:51 +0300

    Update notes screen

--- app/src/main/java/com/example/notekit/home/presentation/NotesScreen.kt
+++ app/src/main/java/com/example/notekit/home/presentation/NotesScreen.kt
@@ -1,13 +1,10 @@
 package com.example.notekit.home.presentation
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -20,15 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.notekit.R
 import com.example.notekit.core.composables.AddItemFAB
 import com.example.notekit.core.composables.AppTopBar
+import com.example.notekit.core.composables.NoteItem
 import com.example.notekit.core.domain.model.Note
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -108,33 +104,6 @@ private fun NotesScreenContent(
                     .padding(horizontal = 16.dp)
                     .clickable { onNoteClick(note.id.toString()) },
                 title = note.name
-            )
-        }
-    }
-}
-
-@Composable
-private fun NoteItem(
-    modifier: Modifier = Modifier,
-    title: String,
-) {
-    Row(
-        modifier = modifier
-            .height(72.dp)
-            .padding(8.dp)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Column(
-            modifier = modifier.padding(start = 4.dp),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = title,
-                color = colorResource(R.color.black),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Medium
             )
         }
     }
