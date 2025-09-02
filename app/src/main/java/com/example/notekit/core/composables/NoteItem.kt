b431868be9f8e884b7dc520a652ee3fdd335fa1f
emyca <evmyca@gmail.com>
2025-09-02 12:32:08 +0300

    Add note item composable

---
+++ app/src/main/java/com/example/notekit/core/composables/NoteItem.kt
@@ -0,0 +1,44 @@
+package com.example.notekit.core.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.notekit.R
+
+@Composable
+fun NoteItem(
+    modifier: Modifier = Modifier,
+    title: String,
+) {
+    Row(
+        modifier = modifier
+            .height(72.dp)
+            .padding(8.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(
+            modifier = modifier.padding(start = 4.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = title,
+                color = colorResource(R.color.black),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
+    }
+}
