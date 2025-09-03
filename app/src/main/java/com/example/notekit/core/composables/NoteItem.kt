8efa6f8bb98236888a12ef92e1853fe0f78a2934
emyca <evmyca@gmail.com>
2025-09-03 09:00:59 +0300

    Add preview

--- app/src/main/java/com/example/notekit/core/composables/NoteItem.kt
+++ app/src/main/java/com/example/notekit/core/composables/NoteItem.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.notekit.R
@@ -41,4 +42,12 @@ fun NoteItem(
             )
         }
     }
+}
+
+@Composable
+@Preview
+private fun NoteItemPreview() {
+    NoteItem(
+        title = "Abcd"
+    )
 }
