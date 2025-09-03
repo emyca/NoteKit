dccc9f3d36dcc81e4b035ee87c03ec3f983056c7
emyca <evmyca@gmail.com>
2025-09-03 13:00:14 +0300

    Update note item

--- app/src/main/java/com/example/notekit/core/composables/NoteItem.kt
+++ app/src/main/java/com/example/notekit/core/composables/NoteItem.kt
@@ -6,16 +6,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.notekit.R
 
 @Composable
 fun NoteItem(
@@ -36,8 +35,8 @@ fun NoteItem(
         ) {
             Text(
                 text = title,
-                color = colorResource(R.color.black),
-                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onSurface,
+                fontSize = 24.sp,
                 fontWeight = FontWeight.Medium
             )
         }
@@ -48,6 +47,6 @@ fun NoteItem(
 @Preview
 private fun NoteItemPreview() {
     NoteItem(
-        title = "Abcd"
+        title = "Lorem ipsum"
     )
 }
