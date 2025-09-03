d56215cd4cc29a1d0050d6f25ae5a4e16d927184
emyca <evmyca@gmail.com>
2025-09-03 12:58:53 +0300

    Update add item fab

--- app/src/main/java/com/example/notekit/core/composables/AddItemFAB.kt
+++ app/src/main/java/com/example/notekit/core/composables/AddItemFAB.kt
@@ -16,9 +16,9 @@ import com.example.notekit.R
 @Composable
 fun AddItemFAB(
     modifier: Modifier = Modifier,
-    containerColor: Color = MaterialTheme.colorScheme.primaryContainer,
-    contentColor: Color = MaterialTheme.colorScheme.primaryContainer,
-    iconTintColor: Color = MaterialTheme.colorScheme.onPrimary,
+    containerColor: Color = MaterialTheme.colorScheme.tertiary,
+    contentColor: Color = MaterialTheme.colorScheme.onTertiary,
+    iconTintColor: Color = MaterialTheme.colorScheme.onTertiary,
     shape: Shape = CircleShape,
     onClick: () -> Unit
 ) {
