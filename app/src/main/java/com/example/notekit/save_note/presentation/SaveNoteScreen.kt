ad6c5b9db6594dc4ea1ec1b0de927f4b5a32c247
emyca <evmyca@gmail.com>
2025-09-06 18:45:20 +0300

    Add save note screen

---
+++ app/src/main/java/com/example/notekit/save_note/presentation/SaveNoteScreen.kt
@@ -0,0 +1,140 @@
+package com.example.notekit.save_note.presentation
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.notekit.R
+import com.example.notekit.core.composables.AppFilledBtn
+import com.example.notekit.core.composables.AppOutlinedTextField
+import com.example.notekit.core.composables.AppTopBar
+
+@Composable
+internal fun SaveNoteScreen(
+    modifier: Modifier = Modifier,
+    uiState: SaveNoteUiState,
+    noteId: Int?,
+    onSaveButtonClick: () -> Unit,
+    navigateToNotesScreen: () -> Unit,
+    onNoteNameChanged: (String) -> Unit,
+    onNoteContentChanged: (String) -> Unit,
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize().imePadding(),
+        topBar = {
+            AppTopBar(
+                title =
+                    if (noteId != null)
+                        stringResource(R.string.update_top_bar_title)
+                    else stringResource(R.string.insert_top_bar_title)
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier.fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            when (uiState) {
+                is SaveNoteUiState.NoteState -> NoteContent(
+                    name = uiState.name,
+                    content = uiState.content,
+                    onNoteNameChanged = onNoteNameChanged,
+                    onNoteContentChanged = onNoteContentChanged,
+                    onSaveButtonClick = onSaveButtonClick,
+                    navigateToNotesScreen = navigateToNotesScreen
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun NoteContent(
+    modifier: Modifier = Modifier,
+    name: String,
+    content: String,
+    onNoteNameChanged: (String) -> Unit,
+    onNoteContentChanged: (String) -> Unit,
+    onSaveButtonClick: () -> Unit,
+    navigateToNotesScreen: () -> Unit
+) {
+    val context = LocalContext.current
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AppOutlinedTextField(
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 36.dp),
+            value = name,
+            labelText = stringResource(id = R.string.name_text_field_label),
+            singleLine = true,
+            onValueChange = onNoteNameChanged
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        AppOutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = content,
+            labelText = stringResource(id = R.string.content_text_field_label),
+            onValueChange = onNoteContentChanged
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        AppFilledBtn(
+            modifier = Modifier
+                .width(160.dp)
+                .height(50.dp),
+            text = stringResource(R.string.save_button_text),
+            onClick = {
+                if (name.isEmpty()) {
+                    Toast.makeText(
+                        context, context.getText(R.string.add_data_toast_text),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return@AppFilledBtn
+                }
+                onSaveButtonClick()
+                navigateToNotesScreen()
+            }
+        )
+    }
+}
+
+@Composable
+@Preview
+private fun SaveNoteScreenPreview() {
+
+    val uiState = SaveNoteUiState
+        .NoteState(
+            1, "My info", "Lorem ipsum dolor sit amet, " +
+                    "consectetur adipiscing elit"
+        )
+
+    SaveNoteScreen(
+        uiState = uiState,
+        noteId = 1,
+        onSaveButtonClick = {},
+        navigateToNotesScreen = {},
+        onNoteNameChanged = {},
+        onNoteContentChanged = {}
+    )
+}
