3c6ddaf47c7068c8fb4200f347992bce06a8eaba
emyca <evmyca@gmail.com>
2025-09-08 10:16:31 +0300

    Add save note route

---
+++ app/src/main/java/com/example/notekit/save_note/presentation/SaveNoteRoute.kt
@@ -0,0 +1,28 @@
+package com.example.notekit.save_note.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+internal fun SaveNoteRoute(
+    modifier: Modifier = Modifier,
+    viewModel: SaveNoteViewModel = hiltViewModel(),
+    topBarTitle: Int?,
+    navigateToNotesScreen: () -> Unit
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    SaveNoteScreen(
+        modifier = modifier,
+        uiState = uiState,
+        topBarTitle = topBarTitle,
+        onSaveButtonClick = { viewModel.handleEvent(SaveNoteUiEvent.OnSaveClicked) },
+        navigateToNotesScreen = navigateToNotesScreen,
+        onNoteNameChanged = { viewModel.handleEvent(SaveNoteUiEvent.OnNoteNameChanged(it)) },
+        onNoteContentChanged = { viewModel.handleEvent(SaveNoteUiEvent.OnNoteContentChanged(it)) }
+    )
+}
