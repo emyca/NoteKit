34010dc2b94c3031c137ffac8ef718312e17f782
emyca <evmyca@gmail.com>
2025-09-08 11:25:19 +0300

    Update app navigation

--- app/src/main/java/com/example/notekit/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/notekit/navigation/AppNavigation.kt
@@ -5,7 +5,11 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.notekit.home.presentation.navigation.NOTES_SCREEN_ROUTE
+import com.example.notekit.home.presentation.navigation.navigateToNotesScreen
 import com.example.notekit.home.presentation.navigation.notesScreen
+import com.example.notekit.save_note.presentation.navigation.SaveNoteResArg
+import com.example.notekit.save_note.presentation.navigation.navigateToSaveNote
+import com.example.notekit.save_note.presentation.navigation.saveNoteScreen
 
 @Composable
 fun AppNavigation() {
@@ -16,8 +20,16 @@ fun AppNavigation() {
         startDestination = NOTES_SCREEN_ROUTE
     ) {
         notesScreen(
-            navigateToAddNoteScreen = {},
-            navigateToDetailNoteScreen = {}
+            navigateToInsertNote = {
+                navController.navigateToSaveNote(
+                    SaveNoteResArg.INSERT_NOTE,null)
+            },
+            navigateToUpdateNote = {
+                navController.navigateToSaveNote(
+                    SaveNoteResArg.UPDATE_NOTE,it)
+            },
         )
+
+        saveNoteScreen { navController.navigateToNotesScreen() }
     }
 }
