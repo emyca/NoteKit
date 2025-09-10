48b3d51481b48beb01e578a9635b1d7dcd954f59
emyca <evmyca@gmail.com>
2025-09-10 17:27:28 +0300

    Add note details route

--- app/src/main/java/com/example/notekit/note_details/presentation/NoteDetailsRoute.kt
+++ app/src/main/java/com/example/notekit/note_details/presentation/NoteDetailsRoute.kt
@@ -1,2 +1,26 @@
 package com.example.notekit.note_details.presentation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+internal fun NoteDetailsRoute(
+    modifier: Modifier = Modifier,
+    viewModel: NoteDetailsViewModel = hiltViewModel(),
+    navigateToNotesScreen: () -> Unit,
+    navigateToUpdateNote: (String) -> Unit,
+) {
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    NoteDetailsScreen(
+        modifier = modifier,
+        uiState = uiState,
+        onIconArrowBackClick = navigateToNotesScreen,
+        onIconDeleteClick = { viewModel.handleEvent(NoteDetailsUiEvent.OnDeleteClick(it)) },
+        onIconEditClick = navigateToUpdateNote,
+    )
+}
