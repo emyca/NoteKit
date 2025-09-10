c73c0a3c22a751c6dd387f7e1ba13c58b1169efe
emyca <evmyca@gmail.com>
2025-09-10 17:27:56 +0300

    Add note details navigation

--- app/src/main/java/com/example/notekit/note_details/presentation/navigation/NoteDetailsNavigation.kt
+++ app/src/main/java/com/example/notekit/note_details/presentation/navigation/NoteDetailsNavigation.kt
@@ -1,2 +1,38 @@
 package com.example.notekit.note_details.presentation.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.notekit.note_details.presentation.NoteDetailsRoute
+
+const val NOTE_DETAILS_ROUTE = "note_details_route"
+const val NOTE_ID_ARG = "note_id"
+
+fun NavController.navigateToNoteDetails(
+    noteId: String?
+) {
+    navigate(
+        route = "$NOTE_DETAILS_ROUTE/$noteId",
+    ) {
+        launchSingleTop = true
+    }
+}
+
+fun NavGraphBuilder.noteDetailsScreen(
+    navigateToNotesScreen: () -> Unit,
+    navigateToUpdateNote: (String) -> Unit
+) {
+    composable(
+        route = "$NOTE_DETAILS_ROUTE/{${NOTE_ID_ARG}}",
+        arguments = listOf(
+            navArgument(NOTE_ID_ARG) { type = NavType.StringType; nullable = true },
+        )
+    ) { backStackEntry ->
+        NoteDetailsRoute(
+            navigateToNotesScreen = navigateToNotesScreen,
+            navigateToUpdateNote = navigateToUpdateNote
+        )
+    }
+}
