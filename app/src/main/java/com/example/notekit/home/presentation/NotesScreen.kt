1e27445683e9e79c8845acc4e4e74565aec7aa7a
emyca <evmyca@gmail.com>
2025-09-15 15:09:38 +0300

    Update notes screen

--- app/src/main/java/com/example/notekit/home/presentation/NotesScreen.kt
+++ app/src/main/java/com/example/notekit/home/presentation/NotesScreen.kt
@@ -1,5 +1,6 @@
 package com.example.notekit.home.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,9 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +21,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -81,6 +85,7 @@ private fun NotesScreenEmpty(modifier: Modifier = Modifier) {
             Text(
                 text = stringResource(R.string.no_data_yet),
                 fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.error
             )
             Spacer(modifier = modifier.height(8.dp))
@@ -100,15 +105,21 @@ private fun NotesScreenContent(
     notes: List<Note>,
     onNoteClick: (String) -> Unit
 ) {
-    LazyColumn(modifier = modifier.fillMaxSize()) {
+    Spacer(modifier = Modifier.height(24.dp))
+    LazyColumn(
+        modifier = modifier
+            .wrapContentSize()
+            .background(MaterialTheme.colorScheme.onPrimary)
+    ) {
         itemsIndexed(notes) { index, note ->
-            Spacer(modifier = Modifier.height(24.dp))
             NoteItem(
                 modifier = Modifier
                     .padding(horizontal = 16.dp)
                     .clickable { onNoteClick(note.id.toString()) },
                 title = note.name
             )
+            if (index < notes.size - 1)
+                HorizontalDivider()
         }
     }
 }
