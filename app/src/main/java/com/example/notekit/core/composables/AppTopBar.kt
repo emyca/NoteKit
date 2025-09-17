9efb3f95c658f73f741bf54313c4cdf76f327849
emyca <evmyca@gmail.com>
2025-09-17 13:59:23 +0300

    Update app top bar

--- app/src/main/java/com/example/notekit/core/composables/AppTopBar.kt
+++ app/src/main/java/com/example/notekit/core/composables/AppTopBar.kt
@@ -2,13 +2,11 @@ package com.example.notekit.core.composables
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.notekit.ui.theme.NoteKitTheme
 
@@ -19,18 +17,17 @@ fun AppTopBar(
     modifier: Modifier = Modifier,
     navigationIcon: @Composable () -> Unit,
     actions: @Composable RowScope.() -> Unit,
-    containerColor: Color = MaterialTheme.colorScheme.onPrimaryContainer,
-    titleContentColor: Color = MaterialTheme.colorScheme.onPrimary,
 ) {
     TopAppBar(
-        title = { Text(title) },
+        title = {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold
+            )
+        },
         modifier = modifier,
         navigationIcon = navigationIcon,
         actions = actions,
-        colors = TopAppBarDefaults.topAppBarColors(
-            titleContentColor = titleContentColor,
-            containerColor = containerColor
-        )
     )
 }
 
@@ -43,8 +40,8 @@ private fun AppTopBarPreview() {
             modifier = Modifier,
             navigationIcon = {},
             actions = {},
-            containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            titleContentColor = MaterialTheme.colorScheme.onPrimary,
+//            containerColor = MaterialTheme.colorScheme.primaryContainer,
+//            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
         )
     }
 }
