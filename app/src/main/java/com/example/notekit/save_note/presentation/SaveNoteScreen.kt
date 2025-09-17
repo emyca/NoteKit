0c390753fa07f0f1525e11bf01082f4e02a83aa7
emyca <evmyca@gmail.com>
2025-09-17 14:02:21 +0300

    Clean code

--- app/src/main/java/com/example/notekit/save_note/presentation/SaveNoteScreen.kt
+++ app/src/main/java/com/example/notekit/save_note/presentation/SaveNoteScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
