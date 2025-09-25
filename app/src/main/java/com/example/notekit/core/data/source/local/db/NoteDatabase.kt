a4e87d6063b13326f9417b7a8cb5fb83005b59ea
emyca <evmyca@gmail.com>
2025-09-25 13:38:51 +0300

    Update app database config

--- app/src/main/java/com/example/notekit/core/data/source/local/db/NoteDatabase.kt
+++ app/src/main/java/com/example/notekit/core/data/source/local/db/NoteDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.notekit.core.data.source.local.dao.NoteDao
 import com.example.notekit.core.data.source.local.model.NoteEntity
 
-@Database(entities = [NoteEntity::class], version = 1)
+@Database(entities = [NoteEntity::class], version = 1, exportSchema = true)
 abstract class NoteDatabase : RoomDatabase() {
     abstract fun noteDao(): NoteDao
 }
