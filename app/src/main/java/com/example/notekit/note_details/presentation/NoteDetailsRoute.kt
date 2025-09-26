ac55b138d85f930c7234c382ef8a7d2a9add58c0
emyca <evmyca@gmail.com>
2025-09-26 17:12:31 +0300

    Update imports due depreceted old ones

--- app/src/main/java/com/example/notekit/note_details/presentation/NoteDetailsRoute.kt
+++ app/src/main/java/com/example/notekit/note_details/presentation/NoteDetailsRoute.kt
@@ -3,7 +3,7 @@ package com.example.notekit.note_details.presentation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
 @Composable
