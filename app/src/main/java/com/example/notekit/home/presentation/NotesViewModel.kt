73f27ba1e053743fca9d3edb92410ed8d1d76823
emyca <evmyca@gmail.com>
2025-09-27 06:22:19 +0300

    Remove unused event

--- app/src/main/java/com/example/notekit/home/presentation/NotesViewModel.kt
+++ app/src/main/java/com/example/notekit/home/presentation/NotesViewModel.kt
@@ -3,15 +3,12 @@ package com.example.notekit.home.presentation
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.notekit.core.domain.model.Note
-import com.example.notekit.home.domain.usecase.DeleteUseCase
 import com.example.notekit.home.domain.usecase.GetAllUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 internal sealed interface NotesScreenUiState {
@@ -20,22 +17,10 @@ internal sealed interface NotesScreenUiState {
     data class Content(val notes: List<Note>) : NotesScreenUiState
 }
 
-internal sealed interface NotesScreenUiEvent {
-    data class OnDeleteClick(val note: Note) : NotesScreenUiEvent
-}
-
 @HiltViewModel
 internal class NotesViewModel @Inject constructor(
-    private val deleteUseCase: DeleteUseCase,
     getAllUseCase: GetAllUseCase
 ) : ViewModel() {
-
-    fun handleEvent(event: NotesScreenUiEvent) {
-        when (event) {
-            is NotesScreenUiEvent.OnDeleteClick ->
-                delete(event.note)
-        }
-    }
 
     private val notes = getAllUseCase()
 
@@ -49,10 +34,4 @@ internal class NotesViewModel @Inject constructor(
             started = SharingStarted.WhileSubscribed(5000),
             initialValue = NotesScreenUiState.Loading
         )
-
-    private fun delete(note: Note) {
-        viewModelScope.launch(Dispatchers.IO) {
-            deleteUseCase(note)
-        }
-    }
 }
