3d5f616a1ca2903ff656bce11976232e455ea3cb
emyca <evmyca@gmail.com>
2025-10-03 18:39:20 +0300

    Add search Note presentation layer

--- app/src/main/java/com/example/notekit/search_note/presentation/SearchNoteScreen.kt
+++ app/src/main/java/com/example/notekit/search_note/presentation/SearchNoteScreen.kt
@@ -1,2 +1,179 @@
 package com.example.notekit.search_note.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.notekit.core.composables.NoteItem
+import com.example.notekit.core.domain.model.Note
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+internal fun SearchNoteScreen(
+    modifier: Modifier = Modifier,
+    viewModel: SearchNoteViewModel = viewModel(),
+    onNoteClick: (String) -> Unit,
+) {
+    val state by viewModel.state.collectAsState()
+    val query by viewModel.query.collectAsState()
+    var queryInput by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
+    Column {
+        val onActiveChange: (Boolean) -> Unit = {}
+        val colors1 = SearchBarDefaults.colors()
+        SearchBar(
+            inputField = {
+                SearchBarDefaults.InputField(
+                    query = query,
+                    onQueryChange = { newQuery ->
+                        viewModel.handleIntent(SearchNoteScreenIntent.UpdateQuery(newQuery))
+                    },
+                    onSearch = { submittedQuery ->
+                        viewModel.handleIntent(SearchNoteScreenIntent.Search(submittedQuery))
+                    },
+                    expanded = true,
+                    onExpandedChange = onActiveChange,
+                    enabled = true,
+                    placeholder = { Text("Search...") },
+                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                    trailingIcon = {
+                        if (active) {
+                            IconButton(onClick = {
+                                if (queryInput.isNotEmpty()) {
+                                    queryInput = ""
+                                    viewModel.handleIntent(SearchNoteScreenIntent.ClearSearch)
+                                } else {
+                                    active = false
+                                }
+                            }) {
+                                Icon(Icons.Default.Clear, contentDescription = "Clear search")
+                            }
+                        }
+                    },
+                    colors = colors1.inputFieldColors,
+                    interactionSource = null,
+                )
+            },
+            expanded = true,
+            onExpandedChange = onActiveChange,
+            modifier = Modifier,
+            shape = SearchBarDefaults.inputFieldShape,
+            colors = colors1,
+            tonalElevation = SearchBarDefaults.TonalElevation,
+            shadowElevation = SearchBarDefaults.ShadowElevation,
+            windowInsets = SearchBarDefaults.windowInsets,
+            content = {
+                when (state) {
+                    is SearchNoteScreenState.Idle -> {
+                        SearchNoteScreenInfo(modifier, "No data yet.")
+                    }
+
+                    is SearchNoteScreenState.Loading -> SearchNoteScreenLoading(modifier)
+                    is SearchNoteScreenState.Success -> {
+                        val results = (state as SearchNoteScreenState.Success).results
+                        SearchNoteScreenContent(modifier, results, onNoteClick = onNoteClick)
+                    }
+
+                    is SearchNoteScreenState.Error -> {
+                        val message = (state as SearchNoteScreenState.Error).message
+                        SearchNoteScreenInfo(modifier, message)
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun SearchNoteScreenLoading(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun SearchNoteScreenInfo(
+    modifier: Modifier = Modifier,
+    info: String
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = info,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colorScheme.error
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchNoteScreenContent(
+    modifier: Modifier = Modifier,
+    notes: List<Note>,
+    onNoteClick: (String) -> Unit
+) {
+    Spacer(
+        modifier
+            .fillMaxWidth()
+            .height(24.dp)
+    )
+    LazyColumn(
+        modifier = modifier
+            .wrapContentSize()
+            .background(MaterialTheme.colorScheme.onPrimary)
+    ) {
+        itemsIndexed(notes) { index, note ->
+            NoteItem(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .clickable { onNoteClick(note.id.toString()) },
+                title = note.name
+            )
+            if (index < notes.size - 1)
+                HorizontalDivider()
+        }
+    }
+}
--- app/src/main/java/com/example/notekit/search_note/presentation/SearchNoteViewModel.kt
+++ app/src/main/java/com/example/notekit/search_note/presentation/SearchNoteViewModel.kt
@@ -1,2 +1,82 @@
 package com.example.notekit.search_note.presentation
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.notekit.core.domain.model.Note
+import com.example.notekit.search_note.domain.usecase.GetNoteByNameUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+internal sealed interface SearchNoteScreenState {
+    object Idle : SearchNoteScreenState
+    object Loading : SearchNoteScreenState
+    data class Success(val results: List<Note>) : SearchNoteScreenState
+    data class Error(val message: String) : SearchNoteScreenState
+}
+
+sealed class SearchNoteScreenIntent {
+    data class UpdateQuery(val query: String) : SearchNoteScreenIntent()
+    data class Search(val query: String) : SearchNoteScreenIntent()
+    data object ClearSearch : SearchNoteScreenIntent()
+}
+
+@OptIn(FlowPreview::class)
+@HiltViewModel
+internal class SearchNoteViewModel @Inject constructor(
+    private val getNoteByNameUseCase: GetNoteByNameUseCase
+) : ViewModel() {
+    private val _state = MutableStateFlow<SearchNoteScreenState>(SearchNoteScreenState.Idle)
+    val state: StateFlow<SearchNoteScreenState> = _state.asStateFlow()
+
+    private val _query = MutableStateFlow("")
+    val query: StateFlow<String> = _query.asStateFlow()
+
+    init {
+        // Debounce search requests
+        viewModelScope.launch {
+            _query.debounce(300L)
+                .filter { it.isNotBlank() }
+                .collect { performSearch(it) }
+        }
+    }
+
+    fun handleIntent(intent: SearchNoteScreenIntent) {
+        when (intent) {
+            is SearchNoteScreenIntent.UpdateQuery -> _query.value = intent.query
+            is SearchNoteScreenIntent.Search -> performSearch(intent.query)
+            is SearchNoteScreenIntent.ClearSearch -> _state.value = SearchNoteScreenState.Idle
+        }
+    }
+
+    private fun performSearch(searchQuery: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _state.value = SearchNoteScreenState.Loading
+            try {
+                val resultsFlow = getNoteByNameUseCase(searchQuery)
+                resultsFlow.map { noteList ->
+                    if (noteList.isNotEmpty())
+                        _state.value = SearchNoteScreenState.Success(noteList)
+                    else SearchNoteScreenState.Idle
+                }.stateIn(
+                    scope = viewModelScope,
+                    started = SharingStarted.WhileSubscribed(5000),
+                    initialValue = SearchNoteScreenState.Loading
+                )
+            } catch (e: Exception) {
+                _state.value = SearchNoteScreenState.Error("Failed to fetch results.")
+            }
+        }
+    }
+}
+
