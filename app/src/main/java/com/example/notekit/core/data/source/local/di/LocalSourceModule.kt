530fde245a598b42a76bbcc98ccfe43b2b5ac324
emyca <evmyca@gmail.com>
2025-10-16 17:02:09 +0300

    Update local source di module

--- app/src/main/java/com/example/notekit/core/data/source/local/di/LocalSourceModule.kt
+++ app/src/main/java/com/example/notekit/core/data/source/local/di/LocalSourceModule.kt
@@ -1,12 +1,14 @@
 package com.example.notekit.core.data.source.local.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import androidx.room.Room
 import com.example.notekit.core.data.repository.NoteRepositoryImpl
 import com.example.notekit.core.data.source.local.LocalDataSource
 import com.example.notekit.core.data.source.local.RoomLocalDataSource
 import com.example.notekit.core.data.source.local.db.NoteDatabase
 import com.example.notekit.core.domain.repository.NoteRepository
+import com.example.notekit.core.utils.SqlCipherKeyManager
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -22,15 +24,32 @@ class LocalSourceModuleProvider {
     @Provides
     fun provideNoteDao(database: NoteDatabase) = database.noteDao()
 
+//    // It has no encryption
+//    @Provides
+//    @Singleton
+//    fun provideLocalDatabase(
+//        @ApplicationContext context: Context
+//    ) = Room.databaseBuilder(
+//        context,
+//        NoteDatabase::class.java,
+//        "note-database"
+//    ).build()
+
     @Provides
     @Singleton
     fun provideLocalDatabase(
         @ApplicationContext context: Context
-    ) = Room.databaseBuilder(
-        context,
-        NoteDatabase::class.java,
-        "note-database"
-    ).build()
+    ): NoteDatabase {
+        System.loadLibrary("sqlcipher")
+        val sharedPreferences = context.getSharedPreferences("app_prefs", MODE_PRIVATE)
+        val sqlCipherKeyManager = SqlCipherKeyManager(sharedPreferences)
+        return Room.databaseBuilder(
+            context,
+            NoteDatabase::class.java,
+            "note-database"
+        ).openHelperFactory(sqlCipherKeyManager.getSupportFactory())
+            .build()
+    }
 }
 
 @Module
