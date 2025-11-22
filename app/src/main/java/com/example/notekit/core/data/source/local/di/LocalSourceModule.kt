54e0520c6b0bf1d1765dd290431ee75c3d057829
emyca <evmyca@gmail.com>
2025-11-22 02:50:55 +0200

    Update local source module

--- app/src/main/java/com/example/notekit/core/data/source/local/di/LocalSourceModule.kt
+++ app/src/main/java/com/example/notekit/core/data/source/local/di/LocalSourceModule.kt
@@ -1,20 +1,21 @@
 package com.example.notekit.core.data.source.local.di
 
 import android.content.Context
-import android.content.Context.MODE_PRIVATE
 import androidx.room.Room
 import com.example.notekit.core.data.repository.NoteRepositoryImpl
 import com.example.notekit.core.data.source.local.LocalDataSource
 import com.example.notekit.core.data.source.local.RoomLocalDataSource
 import com.example.notekit.core.data.source.local.db.NoteDatabase
 import com.example.notekit.core.domain.repository.NoteRepository
-import com.example.notekit.core.utils.SqlCipherKeyManager
+import com.example.notekit.core.utils.SupportFactory
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.util.UUID
+
 import javax.inject.Singleton
 
 @Module
@@ -24,7 +25,7 @@ class LocalSourceModuleProvider {
     @Provides
     fun provideNoteDao(database: NoteDatabase) = database.noteDao()
 
-//    // It has no encryption
+    // It has no encryption
 //    @Provides
 //    @Singleton
 //    fun provideLocalDatabase(
@@ -41,13 +42,13 @@ class LocalSourceModuleProvider {
         @ApplicationContext context: Context
     ): NoteDatabase {
         System.loadLibrary("sqlcipher")
-        val sharedPreferences = context.getSharedPreferences("app_prefs", MODE_PRIVATE)
-        val sqlCipherKeyManager = SqlCipherKeyManager(sharedPreferences)
+        val passphrase =  UUID.randomUUID().toString().toByteArray()
+        val factory = SupportFactory(passphrase)
         return Room.databaseBuilder(
             context,
             NoteDatabase::class.java,
             "note-database"
-        ).openHelperFactory(sqlCipherKeyManager.getSupportFactory())
+        ).openHelperFactory(factory)
             .build()
     }
 }
